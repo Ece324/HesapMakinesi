654fd389fc9edc3fee982c0ea3ce284916170399
Ece <104361726+Ece324@users.noreply.github.com>
2023-07-11 10:31:30 +0300

    1

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "+";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.ToString().Contains('+'))
+            {
+                Hesap_makinesi topla = new Hesap_makinesi();
+                string[] sayilar = textBox1.Text.Split('+');
+                int sonuc = topla.topla(sayilar);
+                textBox1.Text = sonuc.ToString();
+            }
+
+            else if(textBox1.Text.ToString().Contains('-'))
+            {
+                Hesap_makinesi cıkar = new Hesap_makinesi();
+                string[] sayilar = textBox1.Text.Split('-');
+                int sonuc = cıkar.cıkar(sayilar);
+                textBox1.Text = sonuc.ToString();
+            }
+
+            else if(textBox1.Text.ToString().Contains('*')) 
+            {
+                Hesap_makinesi carp = new Hesap_makinesi();
+                string[] sayilar = textBox1.Text.Split('*');
+                int sonuc = carp.carp(sayilar);
+                textBox1.Text = sonuc.ToString();
+            }
+
+            else if (textBox1.Text.ToString().Contains('/'))
+            {
+                Hesap_makinesi bol = new Hesap_makinesi();
+                string[] sayilar = textBox1.Text.Split('/');
+                int sonuc = bol.bol(sayilar);
+                textBox1.Text = sonuc.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "-";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "*";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "/";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WindowsFormsApp1/Hesap-makinesi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Hesap_makinesi
+    {
+        
+        public int topla(string[] sayilar)
+        {
+            int toplam = 0;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                toplam += Convert.ToInt16(sayilar[i]);
+            }
+            return toplam;
+        }
+
+        public int cıkar(string[] sayilar)
+        {
+            int cikar = Convert.ToInt16(sayilar[0]) - Convert.ToInt16(sayilar[1]);
+            for (int i = 2; i < sayilar.Length; i++)
+                {
+                cikar -= Convert.ToInt16(sayilar[i]);
+                    
+                }
+            return cikar;
+        }
+
+        public int carp(string[] sayilar)
+        {
+            int carp = 1;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                carp *= Convert.ToInt16(sayilar[i]);
+            }
+            return carp;
+        }
+
+        public int bol(string[] sayilar)
+        {
+            double bol = Convert.ToInt16(sayilar[0]) / Convert.ToInt16(sayilar[1]);
+            for (int i = 2; i < sayilar.Length; i++)
+            {
+                bol /= Convert.ToInt16(sayilar[i]);
+            }
+            return (int)bol;
+        }
+    }
+}
